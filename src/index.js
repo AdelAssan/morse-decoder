4395c0741f1a16fb399ee3904cdb0aace4a32b97
AdelAssan <adelassanova@gmail.com>
2023-01-22 14:01:15 +0600

    done

--- src/index.js
+++ src/index.js
@@ -38,8 +38,43 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    const words = expr.split('**********');
+   // console.log(words)
+    const res = [];
+    let word = '';
+    let morse = '';
+    let startStat = false;
+    let count = 0;
+    for (let i = 0; i < words.length; i++) {
+      //console.log(words[i])
+      for (let j = 0; j < words[i].length; j++) {
+        //console.log(words[i][j])
+        count += 1;
+        if (!startStat && words[i][j] === '1') {
+          startStat= true;
+        } else if (startStat){
+          if (words[i][j] === '0'){
+            morse += '.';
+          }
+          if (words[i][j] === '1'){
+            morse += '-';
+            //console.log(morse)
+          } 
+          startStat = false;
+        }
+        if (count === 10) {
+          word += MORSE_TABLE[morse];
+          morse = '';
+          count = 0;
+        }
+      }
+      res.push(word);
+      word = '';
+      //console.log(res)
+    }
+    return res.join(' ');
+  }
+   
 
 module.exports = {
     decode
